ed027581505f2c41d297b71cca2ac8514cea652d
dbcorish <davidcorish@outlook.ie>
2021-08-19 02:24:07 +0100

    Implement ViewPager2 and link to TabLayout

--- app/src/main/java/com/dbcorish/robin/activities/HomeActivity.kt
+++ app/src/main/java/com/dbcorish/robin/activities/HomeActivity.kt
@@ -4,29 +4,52 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.dbcorish.robin.R
 import com.dbcorish.robin.databinding.ActivityHomeBinding
 import com.dbcorish.robin.fragments.ActivityFragment
 import com.dbcorish.robin.fragments.HomeFragment
 import com.dbcorish.robin.fragments.MessagesFragment
 import com.dbcorish.robin.fragments.SearchFragment
+import com.google.android.material.tabs.TabLayoutMediator
 import com.google.firebase.auth.FirebaseAuth
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
 
     private val firebaseAuth = FirebaseAuth.getInstance()
-    private val homeFragment = HomeFragment()
-    private val searchFragment = SearchFragment()
-    private val activityFragment = ActivityFragment()
-    private val messagesFragment = MessagesFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val viewPager = binding.viewPager
+        val tabLayout = binding.tabLayout
+
+        val adapter = ViewPagerAdapter(supportFragmentManager, lifecycle)
+        viewPager.adapter = adapter
+
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            when (position) {
+                0 -> {
+                    tab.setIcon(R.drawable.selector_home)
+                }
+                1 -> {
+                    tab.setIcon(R.drawable.selector_search)
+                }
+                2 -> {
+                    tab.setIcon(R.drawable.selector_notifications)
+                }
+                3 -> {
+                    tab.setIcon(R.drawable.selector_messages)
+                }
+            }
+        }.attach()
     }
 
     fun onLogout() {
@@ -35,18 +58,23 @@ class HomeActivity : AppCompatActivity() {
         finish()
     }
 
-    inner class SectionPageAdapter(fm: FragmentManager) : FragmentStateAdapter(fm, lifecycle) {
-        override fun getItemCount() = 3
 
-        override fun createFragment(position: Int): Fragment {
-            return when(position) {
-                0 -> homeFragment
-                1 -> searchFragment
-                2 -> activityFragment
-                else -> messagesFragment
-            }
+    class ViewPagerAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
+        FragmentStateAdapter(fragmentManager, lifecycle) {
+        private val numTabs = 4
+
+        override fun getItemCount(): Int {
+            return numTabs
         }
 
+        override fun createFragment(position: Int): Fragment {
+            return when (position) {
+                0 -> HomeFragment()
+                1 -> SearchFragment()
+                2 -> ActivityFragment()
+                else -> MessagesFragment()
+            }
+        }
     }
 
     companion object {
