1001b9b65b0170a72264c5c01b9d8dbcaedf10fb
dbcorish <davidcorish@outlook.ie>
2021-08-24 01:33:32 +0100

    build.gradle fixes

--- app/src/main/java/com/dbcorish/robin/MainActivity.kt
+++ app/src/main/java/com/dbcorish/robin/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.appcompat.app.AppCompatActivity
  * An activity that inflates a layout that has a NavHostFragment.
  */
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
