733b494a19582a08e5485fc2437ab68b22525a65
dbcorish <davidcorish@outlook.ie>
2021-09-02 01:18:06 +0100

    Tweet fragment xml finished

--- app/src/main/java/com/dbcorish/robin/TweetFragment.kt
+++ app/src/main/java/com/dbcorish/robin/TweetFragment.kt
@@ -1,10 +1,14 @@
 package com.dbcorish.robin
 
+import android.app.Activity
 import android.os.Bundle
+import android.text.InputType
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import androidx.navigation.fragment.navArgs
 import com.dbcorish.robin.databinding.FragmentTweetBinding
 
@@ -25,14 +29,32 @@ class TweetFragment : Fragment() {
         val userID = args.userID
         val userName = args.userName
 
-        binding.textView.text = "My user ID is $userID and username is $userName"
+        binding.tweetText.imeOptions = EditorInfo.IME_ACTION_DONE
+        binding.tweetText.setRawInputType(InputType.TYPE_CLASS_TEXT)
+        binding.tweetText.requestFocus()
+        showKeyboard()
 
-        binding.tweetImage.setOnClickListener {
+        binding.fabPhoto.setOnClickListener {
             addImage()
+        }
+
+        binding.fabSend.setOnClickListener() {
+            sendTweet()
         }
     }
 
     private fun addImage() {
 
+    }
+
+    private fun sendTweet() {
+
+    }
+
+    private fun showKeyboard() {
+        val inputMethodManager =
+            (view?.context ?: return).getSystemService(Activity.INPUT_METHOD_SERVICE)
+                    as InputMethodManager
+        inputMethodManager.showSoftInput(binding.tweetText, InputMethodManager.SHOW_IMPLICIT)
     }
 }
